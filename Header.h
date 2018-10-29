d69af6de0eca7c0676b18906d34fa070ff5a888d
KirillChizhov33 <kirill300999@mail.ru>
2018-10-29 20:39:48 +0300

    The first work

--- Header.h
+++ Header.h
@@ -1,1 +1,18 @@
+#pragma once
+class Stack
+{
+	int Size;
+	int Index;
+	int * Mem;
+	Stack() {};
+public:
+	Stack(int _Size);
+	Stack(const Stack &b);
+	Stack & operator =(const Stack &b);
+	bool IsStackEmpty();
+	bool IsStackFull();
+	void AddElement(int Elem);
+	int DeleteElement();
+	void DeleteAllElements();
+};
 
--- Sourse.cpp
+++ Sourse.cpp
@@ -1,1 +1,60 @@
-Cpp file
+
+#include "Header.h"
+Stack::Stack(int _Size)
+{
+	Size = _Size;
+	Mem = new int[Size];
+	Index = -1;
+}
+Stack::Stack(const Stack &b)
+{
+	Size = b.Size;
+	Index = b.Index;
+	for (int i = 0; i < Index; i++)
+	{
+		Mem[i] = b.Mem[i];
+	}
+}
+Stack & Stack::operator =(const Stack &b)
+{
+	if (this == &b)	return *this;
+	delete[]Mem;
+	Size = b.Size;
+	Index = b.Index;
+	Mem = new int[Size];
+	for (int i = 0; i < Index; i++)
+	{
+		Mem[i] = b.Mem[i];
+	}
+	return *this;
+}
+bool Stack::IsStackEmpty()
+{
+	return (Index == -1);
+}
+bool Stack::IsStackFull()
+{
+	return (Index == Size - 1);
+}
+void Stack::AddElement(int Elem)
+{
+	if (IsStackFull())
+	{
+		throw - 1;
+	}
+	Index++;
+	Mem[Index] = Elem;
+}
+int Stack::DeleteElement()
+{
+	if (IsStackEmpty())
+	{
+		throw - 2;
+	}
+	Index--;
+	return Mem[Index + 1];
+}
+void Stack::DeleteAllElements()
+{
+	Index = -1;
+}
