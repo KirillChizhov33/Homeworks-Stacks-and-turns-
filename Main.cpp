8eefb617dcf89a6012e06cacbeb93bff443535a1
KirillChizhov33 <kirill300999@mail.ru>
2018-10-29 20:28:59 +0300

    The work has been completed

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,91 @@
-Main file
+#define _CRT_SECURE_NO_WARNINGS
+#include "Header.h"
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+int main()
+{
+	int cnt = 0, Sum = 0;
+	Stack Test(100);
+	int op = 0;
+	int First = 0;
+	int Second = 1;
+	while (op != 5)
+	{
+		printf("1.Add(+)\n");
+		printf("2.Subtract(-)\n");
+		printf("3.Undo\n");
+		printf("4.Do\n");
+		printf("5.Exit\n");
+		scanf("%d", &op);
+		switch (op)
+		{
+		case 1:
+			if (Test.IsStackFull())
+			{
+				system("cls");
+				printf("%d\n StackIsFull\n", Sum);
+			}
+			else
+			{
+				printf("Enter the number =");
+				scanf("%d", &cnt);
+				Test.AddElement(cnt, First);
+				Sum = Sum + cnt;
+				Test.DeleteAllElements();
+				system("cls");
+				printf("%d\n", Sum);
+			}
+			break;
+		case 2:
+			if (Test.IsStackFull())
+			{
+				system("cls");
+				printf("%d\n StackIsFull\n", Sum);
+			}
+			else
+			{
+				printf("Enter the number =");
+				scanf("%d", &cnt);
+				Test.AddElement(-cnt, First);
+				Sum = Sum - cnt;
+				Test.DeleteAllElements();
+				system("cls");
+				printf("%d\n", Sum);
+			}
+			break;
+		case 3:
+			if (Test.IsStackEmpty(First))
+			{
+				system("cls");
+				printf("%d\n StackIsEmpty\n", Sum);
+			}
+			else
+			{
+				cnt = Test.DeleteElement(First);
+				Test.AddElement(cnt, Second);
+				Sum = Sum - cnt;
+				system("cls");
+				printf("%d\n", Sum);
+			}
+			break;
+		case 4:
+			if (Test.IsStackEmpty(Second))
+			{
+				system("cls");
+				printf("%d\nStackIsEmpty\n", Sum);
+			}
+			else
+			{
+				cnt = Test.DeleteElement(Second);
+				Test.AddElement(cnt, First);
+				Sum = Sum + cnt;
+				system("cls");
+				printf("%d\n", Sum);
+			}
+			break;
+		}
+	}
+	return 0;
+}
+
--- Sourse.cpp
+++ Sourse.cpp
@@ -1,1 +1,82 @@
-Cpp file
+#include "Header.h"
+Stack::Stack(int _Size)
+{
+	Size = _Size;
+	Mem = new int[Size];
+	LowIndex = -1;
+	HighIndex = Size;
+}
+Stack::Stack(const Stack & b)
+{
+	Size = b.Size;
+	Mem = new int[Size];
+	LowIndex = b.LowIndex;
+	HighIndex = b.HighIndex;
+	for (int i = 0; i < LowIndex; i++)
+	{
+		Mem[i] = b.Mem[i];
+	}
+	for (int j = HighIndex; j < Size; j++)
+	{
+		Mem[j] = b.Mem[j];
+	}
+}
+bool Stack::IsStackEmpty(int Pointer)
+{
+	if (Pointer != 0 && Pointer != 1)
+	{
+		throw - 1;
+	}
+	if (Pointer == 0) return LowIndex == -1;
+	if (Pointer == 1) return HighIndex == Size - 1;
+}
+bool Stack::IsStackFull()
+{
+	return (LowIndex + 1 == HighIndex);
+}
+void Stack::AddElement(int Elem, int Pointer)
+{
+	if (IsStackFull())
+	{
+		throw - 2;
+	}
+	if (Pointer == 0)
+	{
+		LowIndex++;
+		Mem[LowIndex] = Elem;
+	}
+	if (Pointer == 1)
+	{
+		HighIndex--;
+		Mem[HighIndex] = Elem;
+	}
+	if (Pointer != 0 && Pointer != 1)
+	{
+		throw - 3;
+	}
+}
+int Stack::DeleteElement(int Pointer)
+{
+	if (IsStackEmpty(Pointer))
+	{
+		throw - 4;
+	}
+	if (Pointer == 0)
+	{
+		LowIndex--;
+		return Mem[LowIndex + 1];
+	}
+	if (Pointer == 1)
+	{
+		HighIndex++;
+		return Mem[HighIndex - 1];
+	}
+	if (Pointer != 0 && Pointer != 1)
+	{
+		throw - 3;
+	}
+}
+void Stack::DeleteAllElements()
+{
+	HighIndex = Size - 1;
+}
