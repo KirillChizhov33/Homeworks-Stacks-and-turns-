891580e4498c37b9bbc0952982d89b8782a6c780
KirillChizhov33 <kirill300999@mail.ru>
2018-10-29 20:25:10 +0300

    Work complete

--- Header.h
+++ Header.h
@@ -1,1 +1,18 @@
+#pragma once
+class Stack
+{
+	int Size;
+	int LowIndex;
+	int HighIndex;
+	int * Mem;
+public:
+	Stack(int _Size);
+	Stack(const Stack & b);
+	bool IsStackEmpty(int Pointer);
+	bool IsStackFull();
+	void AddElement(int Elem, int Pointer);
+	int DeleteElement(int Pointer);
+	void DeleteAllElements();
+};
+
 
