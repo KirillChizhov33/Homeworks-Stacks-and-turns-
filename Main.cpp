2f4a786de7b6d5291c218b1200a1ea9a0d6dd49d
KirillChizhov33 <42999419+KirillChizhov33@users.noreply.github.com>
2018-11-14 13:51:49 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,7 +4,7 @@ int main()
 	Turn CPU1(100);
 	Turn CPU2(100);
 	Turn A(100);
-	Task temp1, temp2;
+	Task temp1, temp2, M;
 	int k = 1;
 	for (int i = 0; i < 100 - 1; i++)
 	{
@@ -24,6 +24,7 @@ int main()
 			{
 				temp1 = A[i];
 				CPU1.AddElement(temp1);
+				temp1.TimeAdd(); //Если выборка задачи занимает 1 такт
 			}
 			if (temp1.GetTime() == 0)
 			{
@@ -32,11 +33,24 @@ int main()
 			}
 			if (CPU2.IsTurnEmpty())
 			{
-				temp2 = A[i];
+				if (i == 0)
+				{
+					temp2 = A[i + 1];
+					i++;
+				}
+				else
+				if (A[i] == temp2)
+				{
+					temp2 = A[i + 1];
+					i++;
+				}
+				else temp2 = A[i];
 				CPU2.AddElement(temp2);
+				temp2.TimeAdd();
 			}
 			if (temp2.GetTime() == 0)
 			{
+				M = temp2;
 				CPU2.DeleteElement();
 				break;
 			}
@@ -48,4 +62,3 @@ int main()
 	}
 	return 0;
 }
-
